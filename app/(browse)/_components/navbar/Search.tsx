d1190d41046a3daaf144d0277c20ea149d70d616
Jahangeer Ansari <jahangeeransari194@gmail.com>
2024-11-20 22:31:16 +0530

    we have create one serchbar with search fucntionality search with query string

---
+++ app/(browse)/_components/navbar/Search.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+// every thing insdide render search will be client comp
+// but if we use {children} we can also render the server comp\
+import qs from "query-string";
+import { Input } from "@/components/ui/input";
+import { SearchIcon } from "lucide-react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export const Search = () => {
+  const router = useRouter();
+  const [value, setValue] = useState("");
+  const handleSearchForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!value) return;
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: {
+          term: value,
+        },
+      },
+      { skipEmptyString: true }
+    );
+    //localhost:3000?term=value
+    router.push(url);
+  };
+  return (
+    <form
+      onSubmit={handleSearchForm}
+      className="relative w-full lg:w-[400px] flex items-center outline-none"
+    >
+      <Input
+        value={value}
+        placeholder="Search"
+        onChange={(e) => setValue(e.target.value)}
+        className="rounded-r-none"
+      />
+      <Button
+        type="submit"
+        size="sm"
+        variant="secondary"
+        className="rounded-l-none"
+      >
+        <SearchIcon className="text-muted-foreground h-5 w-5 text-black" />
+      </Button>
+    </form>
+  );
+};
+
+// server comp
+// export const Search = () => {
+//   return <div>{children}</div>;
+// };
