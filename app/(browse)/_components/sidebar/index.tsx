6ab7cdc2be7e7d366fe9620fb8c6dae97b1019a8
Jahangeer Ansari <jahangeeransari194@gmail.com>
2024-11-21 21:45:38 +0530

    how to inject server component inot client component

---
+++ app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,9 @@
+import { Wrapper } from "./wrapper"
+
+export const Sidebar = () => {
+    // wrapper will be client comp 
+    // sidebar will be server comp i will inject server comp into clinet comp
+    return <Wrapper>
+          sidebar
+    </Wrapper>;
+}
