9fb1987e012254d9fdcb150c511e507f309ac8b7
Jahangeer Ansari <jahangeeransari194@gmail.com>
2024-11-22 20:30:15 +0530

    we have fix the homepage side with the sidebar onCollapsw and onExpand

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,17 +1,21 @@
+import { Container } from "./_components/container";
 import { Navbar } from "./_components/navbar";
 import { Sidebar } from "./_components/sidebar";
 
 const BrowseLayout = ({ children }: { children: React.ReactNode; }) => {
     return (
-        <>
-        <Navbar/>
-            <div className="flex h-full pt-20"> 
-                {/*  ṣidebar*/}
-                <Sidebar/>
-            {children}
+      <>
+        <Navbar />
+        <div className="flex h-full pt-20">
+          {/*  ṣidebar*/}
+                <Sidebar />
+                {/* this is containr for fixed size for homepage
+                this children belong to homepage we ca adjust the homepage size of sidebar
+                */}
+          <Container>{children}</Container>
         </div>
-        </>
-      );
+      </>
+    );
 }
  
 export default BrowseLayout;
