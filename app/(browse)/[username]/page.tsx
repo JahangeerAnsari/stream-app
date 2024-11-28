52697bf4d55451b494d26f0b655f66e68bdb76df
Jahangeer Ansari <jahangeeransari194@gmail.com>
2024-11-28 18:19:47 +0530

    created follow module and render following the user

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,9 +1,27 @@
+import { isFollowingUser } from "@/lib/follow-service";
+import { getUserByUsername } from "@/lib/user-service";
+import { notFound } from "next/navigation";
+
+
 interface UserNameProps{
     params: {
         username: string;
  }
 }
-const UserPage = ({params}: UserNameProps) => {
-    return <div>User Page {params.username}</div>;
+const UserPage = async ({ params }: UserNameProps) => {
+    
+    const user = await getUserByUsername(params.username);
+    if (!user) {
+        notFound()
+    }
+    const isfollowing = await isFollowingUser(user?.id);
+    
+    
+    return (
+      <div>
+        User Page {params.username} {user?.id}
+        following {JSON.stringify(isfollowing)}
+      </div>
+    );
 };
 export default UserPage;
