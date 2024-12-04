5fc02affebab852220976b00d443b065015b6234
Jahangeer Ansari <jahangeeransari194@gmail.com>
2024-12-04 21:43:59 +0530

    we have added navlist items and navlist skeleton for it

--- app/(dashboard)/u/[username]/_components/sidebar/index.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/index.tsx
@@ -1,10 +1,11 @@
 import { Toggle } from "./toggle"
 import { Wrapper } from "./wrapper"
-
+import { Navigation} from './navigation'
 export const Sidebar = () => {
     return (
         <Wrapper>
-            <Toggle/> 
+            <Toggle /> 
+            <Navigation/>
         </Wrapper>
     )
 }
--- app/(dashboard)/u/[username]/_components/sidebar/wrapper.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/wrapper.tsx
@@ -9,7 +9,7 @@ interface WrapperProps {
 export const Wrapper = ({ children }: WrapperProps) => {
     const {collapsed} = useCreatorSidebar((state) => state)
     return (
-        <aside className={cn("fixed flex left-0 flex-col w-60 h-full bg-background  border-r border-[#202E35] Z-50",collapsed && "w-[70px]" )}>
+        <aside className={cn("fixed flex left-0 flex-col w-[70px] lg:w-60 h-full bg-background  border-r border-[#202E35] Z-50",collapsed && "lg:w-[70px]" )}>
             {children}
         </aside>
     )
